34a066cb6a13d1551371d27aae4be7402e4a88a9
Stéphane Dauguet <sdauguet@gmail.com>
2021-12-13 14:22:56 +0100

    end lesson 07 - generics

--- src/lessons/07_types_generiques.ts
+++ src/lessons/07_types_generiques.ts
@@ -84,3 +84,149 @@ const addItemToCollectionInterface: AddItemFunction = <T>(
 ): T[] => {
   return [...collection, item];
 };
+
+// 03 - Classes et types génériques - **********
+
+class Stack {
+  // sans générique
+  items: any[] = [];
+  push(item) {
+    this.items.push(item);
+  }
+  pop() {
+    return this.items.pop();
+  }
+  display() {
+    console.log("Items : ", this.items);
+  }
+}
+
+const newStackNumber = new Stack();
+newStackNumber.push(1);
+newStackNumber.display();
+newStackNumber.push(2);
+newStackNumber.display();
+const myNumber = newStackNumber.pop();
+newStackNumber.display();
+console.log("My number : ", myNumber);
+
+const newStackString = new Stack();
+newStackString.push("Coucou ");
+newStackString.display();
+newStackString.push("le Monde ");
+newStackString.display();
+const myString = newStackString.pop();
+newStackString.display();
+console.log("My string : ", myString);
+
+class StackT<T> {
+  // Avec générique
+  items: T[] = [];
+  push(item: T): void {
+    this.items.push(item);
+  }
+  pop(): T {
+    return this.items.pop();
+  }
+  display(): void {
+    console.log("Items : ", this.items);
+  }
+}
+
+const newStackStringT = new StackT<string>();
+newStackStringT.push("Hello ");
+newStackStringT.display();
+newStackStringT.push("World ");
+newStackStringT.display();
+const myStringT = newStackStringT.pop();
+newStackStringT.display();
+console.log("My string : ", myStringT);
+
+// 04 - Contraintes - **********
+
+interface Name {
+  name: string;
+}
+
+function displayName<T extends Name>(object: T) {
+  console.log(object.name);
+}
+
+function displayProp<T, U extends keyof T>(object: T, prop: U) {
+  console.log(prop, object[prop]);
+}
+
+displayProp({ price: 50 }, "price");
+
+// 05 - Collections et types génériques - **********
+
+interface User5 {
+  username: string;
+}
+
+// Readonly
+const newUser5: Readonly<User5> = {
+  username: "Charlie",
+};
+
+// newUser5.username = "Lilou";  // Not ok
+
+// Partial
+interface User5_1 {
+  username: string;
+  age: number;
+  address: {
+    city: string;
+  };
+}
+
+const newUser5_1: User5_1 = {
+  username: "Lilou",
+  age: 24,
+  address: {
+    city: "Rennes",
+  },
+};
+
+function editUser(user: User5_1, editedUser: Partial<User5_1>) {
+  return { ...user, ...editedUser };
+}
+
+editUser(newUser5_1, { age: 20 });
+
+// Record
+interface Page {
+  title: string;
+}
+
+type PageType = "home" | "about" | "contact";
+
+const pages: Record<PageType, Page> = {
+  home: {
+    title: "homepage",
+  },
+  about: {
+    title: "about",
+  },
+  contact: {
+    title: "contact",
+  },
+};
+
+// Pick
+interface User5_2 {
+  username: string;
+  age: number;
+  address: {
+    city: string;
+  };
+}
+type LightUser = Pick<User5_2, "username" | "age">;
+
+const newUser5_2: LightUser = {
+  username: "Lilou",
+  age: 24,
+};
+
+// Omit
+type LightUser2 = Omit<User5_2, "address">;
